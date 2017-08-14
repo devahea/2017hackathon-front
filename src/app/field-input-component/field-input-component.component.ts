96506e6d8341fe69b2f843906ba9333f18ee1bf4
김민수 <viviennes7@naver.com>
2017-08-14 14:46:59 +0900

    add field-input

---
+++ src/app/field-input-component/field-input-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-field-input-component',
+  templateUrl: './field-input-component.component.html',
+  styleUrls: ['./field-input-component.component.css']
+})
+export class FieldInputComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
