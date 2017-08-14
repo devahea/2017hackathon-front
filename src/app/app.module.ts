2dc343cacd9102ee8a36e7eaef23a95234ea1032
김종인 <kji6252@gmail.com>
2017-08-14 18:42:34 +0900

    레이아웃 생성

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SetOptionComponentComponent } from './set-option-component/set-option-component.component';
@@ -18,7 +19,8 @@ import { FieldInputWrapperComponent } from './field-input-wrapper-component/fiel
   imports: [
     BrowserModule,
     NgbModule.forRoot(),
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/set-option-component/set-option-component.component.ts
+++ src/app/set-option-component/set-option-component.component.ts
@@ -13,15 +13,20 @@ export class SetOptionComponentComponent implements OnInit {
 
 
   constructor(private http: HttpClient) {
-    this.http.get('localhost:8080/category_type').subscribe(data => {
+    this.http.get('http://localhost:8080/category_type').subscribe(data => {
       // Read the result field from the JSON response.
-      console.log(JSON.stringify(data));
-      this.categoryTypes = data['results'];
+      console.log(data);
     });
 
   }
 
   ngOnInit()  {
+
+  }
+
+  submitData(){
+
+    console.log(this);
 
   }
 
