f93eefb3217f0dfad86a0e78bb7bdb485e245d9f
김종인 <kji6252@gmail.com>
2017-08-14 15:02:38 +0900

    부트스트랩4 예제추가

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { SetOptionComponentComponent } from './set-option-component/set-option-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SetOptionComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
